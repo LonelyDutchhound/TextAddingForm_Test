3195a13b5e0fc62daf549ebf77a296a9f0d42cb4
Lonely Dutchhound <lonely.dutchhound@gmail.com>
2019-08-19 15:18:14 +0300

    posts submit

--- text-add.js
+++ text-add.js
@@ -1,23 +1,29 @@
 $(function functionName() {
 
-  $('.text-form__add-button')[0].addEventListener('click', addNewUserText);
+  $('.text-form__add-button')[0].addEventListener('click', addPost);
 
 });
 
-function addNewUserText () {
+function addPost () {
+ if ( $('.text-input__field').val() !== "" && $('.date-input__field').val() !== "") {
+    userText = $('.text-input__field').val();
+    userDate = $('.date-input__field').val();
 
-  userText = $('.text-input__field').val();
-  userDate = $('.date-input__field').val();
+    $('.text-input__field,.date-input__field').val('');
 
-  $('.text-input__field,.date-input__field').val('');
+    let newUserPost = new UserPost (userText, userDate);
 
-  let newUserPost = new UserPost (userText, userDate);
-
-  console.log(newUserPost);
-  $('text-form__show-text-group').
-}
+    $('.text-form__show-text-group').prepend(newUserPost.createPost());
+  }
+};
 
 function UserPost ( userText, userDate ) {
   this.userText = userText,
-  this.userDate = userDate
-}
+  this.userDate = userDate,
+  this.createPost = function () {
+    const checkBox = '<div class="user-post__checkbox"><input type="checkbox" id="' + this.userDate + '"></div>';
+    let postString = '<div class="user-post_text"><p><i>' + this.userDate + '</i><br>' + this.userText + '</p></div>';
+    let postHtml = '<div class="user-post__block">' + checkBox + postString + '</div>'
+    return postHtml;
+  }
+};
