460897c98f4dd69dacf9875a11db09a4950b6dc4
Lonely Dutchhound <lonely.dutchhound@gmail.com>
2019-08-20 14:57:05 +0300

    beautify

--- text-add.js
+++ text-add.js
@@ -1,44 +1,38 @@
 $(function functionName() {
-
   $('.text-form__add-button')[0].addEventListener('click', addPost);
-  $('.text-form__add-group').keyup(function () {
+  $('.text-form__add-group').keyup(function() {
     if (event.keyCode === 13) addPost();
-  });
-
+    });
 });
 
-function addPost () {
+function addPost() {
   const $textfield = $('.text-input__field');
   const $datefield = $('.date-input__field');
 
-  if ( $textfield.val() && $datefield.val() ) {
+  if ($textfield.val() && $datefield.val()) {
+    const userText = $textfield.val();
+    const userDate = $datefield.val();
+    let newUserPost = new UserPost(userText, userDate);
 
-      const userText = $textfield.val();
-      const userDate = $datefield.val();
+    newUserPost.createDateContainer();
+    newUserPost.createPost();
 
-      let newUserPost = new UserPost (userText, userDate);
-
-      newUserPost.createDateContainer();
-      newUserPost.createPost();
-
-      $('.text-input__field,.date-input__field').val('');
-      $('.text-form__show-text-group')[0].addEventListener('change', sortPosts);
-
+    $('.text-input__field,.date-input__field').val('');
+    $('.text-form__show-text-group')[0].addEventListener('change', sortPosts);
   } else {
-
     $('.preq_error').addClass('show')
   }
 };
 
-function UserPost ( userText, userDate ) {
+function UserPost(userText, userDate) {
   this.userText = userText,
-  this.userDate = userDate.substring(0, 10) + ' ' + userDate.substring(11),
-  this.uniqId = userDate.split('-').join('').split(':').join('');
+    this.userDate = userDate.substring(0, 10) + ' ' + userDate.substring(11),
+    this.uniqId = userDate.split('-').join('').split(':').join('');
 
   this.createDateContainer = () => {
     if ($('.preq_error').length) $('.preq_error').removeClass('show');
     if (!$(`.n${this.uniqId.substring(0, 8)}`).length)
-    $('.text-form__show-text-group').prepend(`
+      $('.text-form__show-text-group').prepend(`
       <div class="user-post__date-block  n${this.uniqId.substring(0, 8)}">
         <p class="block-date">Post from ${this.userDate}</p>
       </div>`);
@@ -54,11 +48,10 @@ function UserPost ( userText, userDate ) {
       </div><div class="user-post_text">
         <p><em>${this.userDate}</em><br>${this.userText}</p>
       </div>`);
-  }
+  };
 };
 
 function sortPosts(event) {
   const postId = event.target.id;
-
-  $('.'+ postId).toggleClass('selected');
-}
+  $('.' + postId).toggleClass('selected');
+};
