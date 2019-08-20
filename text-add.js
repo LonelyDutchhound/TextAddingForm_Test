d8ad71b3bc91f91f74fe2e239b5787bfbc886b91
Lonely Dutchhound <lonely.dutchhound@gmail.com>
2019-08-20 03:33:16 +0300

    refactoring: template strings, em instead of i time-bug fixed sorting in range of date

--- text-add.js
+++ text-add.js
@@ -7,33 +7,34 @@ $(function functionName() {
 function addPost () {
  if ( $('.text-input__field').val() !== "" && $('.date-input__field').val() !== "") {
 
-    let userText = $('.text-input__field').val();
-    let userDate = $('.date-input__field').val();
+    const userText = $('.text-input__field').val();
+    const userDate = $('.date-input__field').val();
+    const newUserPost = new UserPost (userText, userDate);
 
     $('.text-input__field,.date-input__field').val('');
 
-    let newUserPost = new UserPost (userText, userDate);
-
-    $('.text-form__show-text-group').prepend(newUserPost.createPost());
-
     $('.text-form__show-text-group')[0].addEventListener('change', sortPosts);
+    newUserPost.createDateContainer();
+    newUserPost.createPost();
   }
 };
 
 function UserPost ( userText, userDate ) {
   this.userText = userText,
-  this.userDate = userDate.substring(0, 10) + ' ' + userDate.substring(12),
+  this.userDate = userDate.substring(0, 10) + ' ' + userDate.substring(11),
   this.uniqId = userDate.split('-').join('').split(':').join('');
-  this.createPost = function () {
-    const checkBox = '<div class="user-post__checkbox"><input type="checkbox" id="n' + this.uniqId + '"></div>';
-    let postString = '<div class="user-post_text"><p><i>' + this.userDate + '</i><br>' + this.userText + '</p></div>';
-    let postHtml = '<div class="user-post__block  n' + this.uniqId +'">' + checkBox + postString + '</div>'
-    return postHtml;
+  this.createDateContainer = () => {
+    if ($(`.n${this.uniqId.substring(0, 8)}`).length === 0)
+    $('.text-form__show-text-group').prepend(`<div class="user-post__date-block  n${this.uniqId.substring(0, 8)}"><p class="block-date">Post from ${this.userDate}</p></div>`);
+  }
+  this.createPost = () => {
+    $(`.n${this.uniqId.substring(0, 8)}`).append(`<div class="user-post__block  n${this.uniqId}"></div>`);
+    $(`.n${this.uniqId}`).prepend(`<div class="user-post__checkbox"><input type="checkbox" id="n${this.uniqId}"></div><div class="user-post_text"><p><em>${this.userDate}</em><br>${this.userText}</p></div>`);
   }
 };
 
 function sortPosts() {
-  let postId = window.event.target.id;
-  
+  const postId = event.target.id;
+
   $('.'+ postId).toggleClass('selected');
 }
